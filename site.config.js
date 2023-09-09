5e288472fa6627d909795cf583b8d8ee16b009d8
Canyu <volcverse@gmail.com>
2023-09-09 15:41:33 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "Canyu",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "CAD&CG Lab, ZJU",
-    bio: "A noob",
+    bio: "Student",
     email: "volcverse@gmail.com",
     linkedin: "volcverse",
     github: "volcverse",
